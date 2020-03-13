5ee4aa744fc3f36846e3ee5c9d709205b598239a
song <songeun.you7@gmail.com>
2020-03-12 22:31:58 -0400

    Blank template

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # letter-to-myself
-Type a letter to yourself, it will replace negative words with positive ones so that in the end you've written an incredible love letter to yourself. 
+Type a letter to yourself, it will replace negative words with positive ones so that in the end you've written an incredible love letter to yourself.
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    Blank State
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
