dd14cac09eea229998b128fea3f5f404252f1864
song <songeun.you7@gmail.com>
2020-04-07 23:32:32 -0400

    Added parallax heading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './css/App.css';
 
+import letter from './media/letter.jpg';
+
 import Input from './components/Input';
 
 class App extends Component {
@@ -12,12 +14,40 @@ class App extends Component {
         };
     }
 
+    componentDidMount() {
+        var title = document.getElementById("title");
+        var photo = document.getElementById("photo");
+        var subtitle = document.getElementById("subtitle");
+
+
+        document.onmousemove = function(event) {
+            var x = (event.clientX / window.innerWidth) * 6;
+            var y = (event.clientY / window.innerHeight) * 18;
+
+            // var x2 = (event.clientX / window.innerWidth) * 1.05 - 5;
+            // var y2 = (event.clientY / window.innerHeight) * 1.6 - 5;
+
+
+            var x3 = (event.clientX / window.innerWidth) * 1.5 - 2;
+            var y3 = (event.clientY / window.innerHeight) * 8 - 6;
+
+            title.style.transform = "translate("+x+"%,"+y+"%)";
+            // photo.style.transform = "translate("+x2+"%,"+y2+"%)";
+            subtitle.style.transform = "translate("+x3+"%,"+y3+"%)";
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>
-                    Sometimes we're too hard on ourselves. Write out your negative self talk—let's try to change it up.
-                </h1>
+                <div className="parallax-box">
+                    <h1 id="title">Letter to Myself</h1>
+                    <div id="letter-box">
+                        <img id="photo" src={letter}/>
+                        <h2 id="subtitle">Sometimes we're too hard on ourselves. Write out your negative self talk—let's try to change it up</h2>
+                        <div className="gradient-box"/>
+                    </div>
+                </div>
                 <Input/>
             </div>
         );
