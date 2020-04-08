c7265f0df201fdc988c46d9c24a4b27068bd06b7
song <songeun.you7@gmail.com>
2020-04-08 04:47:51 -0400

    Rewrote explanation text, formatted

--- src/components/Info.js
+++ src/components/Info.js
@@ -15,16 +15,17 @@ class Info extends Component {
         return (
             <div className={"info-box " + `${this.props.visible ? "visible" : "invisible"}`}>
                 <div className="description">
-                    Type a letter to yourself, it will replace negative words with positive ones so that in the end you've written an incredible love letter to yourself.
+                    <p>You know you deserve compassion, but it's difficult to tell yourself kind things when you're in a bad place.</p>
+                    <p>It's okay. Say the hurtful, ugly things. The negative words with be replaced with positive ones so that in the end you've written an incredible love letter to yourself.</p>
                 </div>
 
                 <div className="row">
                     <div className="before">
-                        <p className="title">NEGATIVE</p>
+                        <p className="title">What you type:</p>
                         I felt pretty bad today. I hate how I look and it made me feel really anxious to go outside. I don't want people seeing my ugly face and fat body. I just want to give up.
                     </div>
                     <div className="after">
-                        <p className="title">POSITIVE</p>
+                        <p className="title">What you mean to say:</p>
                         I felt pretty <div className="inline good">good</div> today. I <div className="inline love">love</div> how I look and it made me feel really <div className="inline secure">secure</div> to go outside. I want people seeing my <div className="inline beautiful">beautiful</div> face and <div className="inline good-looking">good-looking</div> body. I want to <div className="inline persevere">persevere</div>.
                     </div>
                 </div>
